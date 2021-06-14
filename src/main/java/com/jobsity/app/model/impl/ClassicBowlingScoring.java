4e8602c0ae63e4ec93f03e0369ba4671c78a0ce6
Santiago Hyun <santiago.hyun@inxeption.com>
2021-06-13 19:45:53 -0500

    Updating: Classic Bowling scoring logic first version. Adding comments

--- src/main/java/com/jobsity/app/model/impl/ClassicBowlingScoring.java
+++ src/main/java/com/jobsity/app/model/impl/ClassicBowlingScoring.java
@@ -36,7 +36,6 @@ public class ClassicBowlingScoring implements GameScoring {
                 calculateTotalScoringByFrame(frame, frameNext, frameNextNext, framePrevious);
                 index++;
             }
-            System.out.println("Player '" + player.getName() + "': " + player.getFrames().getLast().getTotalScore());
         });
 
     }
@@ -44,38 +43,50 @@ public class ClassicBowlingScoring implements GameScoring {
     private void calculateTotalScoringByFrame(Frame frame, Frame frameNext, Frame frameNextNext, Frame framePrevious) {
         int prevTotal = framePrevious != null ? framePrevious.getTotalScore() : 0;
         int currentFrameTotal = prevTotal + 0;
-        // from 0 to 8 frame
+        // frames: 0 - antepenultimate
         if (frame != null && frameNext != null && frameNextNext != null) {
             if (frame.isStrike()) {
+                // if current is strike check the next
                 if (frameNext.isStrike()) {
+                    // if next also is strike check the second next, sum current subTotalScore + next subTotalScore
                     currentFrameTotal = currentFrameTotal + frame.getSubTotalScore() + frameNext.getSubTotalScore();
                     if (frameNextNext.isStrike()) {
+                        // if second next also is strike, sum subTotalScore from second next
                         currentFrameTotal = currentFrameTotal + frameNextNext.getSubTotalScore();
                     } else {
+                        // if second next is not strike, sum ball1 from second next
                         currentFrameTotal = currentFrameTotal + Utils.convertRollStringToInt(frameNextNext.getBall1());
                     }
                 } else {
+                    // if next is not strike, sum subTotalScore from next + current subTotalScore
                     currentFrameTotal = currentFrameTotal + frame.getSubTotalScore() + frameNext.getSubTotalScore();
                 }
             } else if (frame.isSpare()) {
+                // if current is stripe, sum ball1 from next + current subTotalScore
                 currentFrameTotal = currentFrameTotal + frame.getSubTotalScore()
                         + Utils.convertRollStringToInt(frameNext.getBall1());
             } else {
+                // if current is not stripe/strike, sum current subTotalScore
                 currentFrameTotal = currentFrameTotal + frame.getSubTotalScore();
             }
-            // 9 frame
+        // frame: penultimate
         } else if (frame != null && frameNext != null && frameNextNext == null) {
             if (frame.isStrike()) {
+                // if current is strike, sum ball1,ball2 from next + current subTotalScore
                 currentFrameTotal = currentFrameTotal + frame.getSubTotalScore()
                         + Utils.convertRollStringToInt(frameNext.getBall1())
                         + Utils.convertRollStringToInt(frameNext.getBall2());
             } else if (frame.isSpare()) {
+                // if current is spare, sum ball1 from next + current subTotalScore
                 currentFrameTotal = currentFrameTotal + frame.getSubTotalScore()
                         + Utils.convertRollStringToInt(frameNext.getBall1());
             } else {
+                // if current is not spare/strike, sum current subTotalScore
                 currentFrameTotal = currentFrameTotal + frame.getSubTotalScore();
             }
+        // frame: last
         } else {
+            // if current is the last frame, sum current subTotalScore
             currentFrameTotal = currentFrameTotal + frame.getSubTotalScore();
         }
         frame.setTotalScore(currentFrameTotal);
